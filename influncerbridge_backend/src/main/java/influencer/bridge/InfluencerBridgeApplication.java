e8991f02912d23f9b94f406cee6ec4051946e096
SUKANT <sukantkrishna7303@gmail.com>
2023-10-27 00:21:38 +0530

    backend

---
+++ influncerbridge_backend/src/main/java/influencer/bridge/InfluencerBridgeApplication.java
@@ -0,0 +1,13 @@
+package influencer.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencerBridgeApplication.class, args);
+	}
+
+}
